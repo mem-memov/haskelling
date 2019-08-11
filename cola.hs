df897bf5acafd7687b68478eec6759c4d5894ae7
Alexander Novikov <mem-memov@yandex.ru>
2019-08-11 03:06:42 +0300

    qa

--- cola.hs
+++ cola.hs
@@ -1,20 +1,48 @@
+import System.Directory
+
+data Fan a b = ClosedFan | A a | B b | Fan [Fan a b] [Fan a b] deriving (Show, Read)
 type Words = String
-data Question = Question Words [Answer] deriving Show
-data Answer = Answer Words [Question] deriving Show
+data Question = Question Words deriving (Show, Read)
+data Answer = Answer Words deriving (Show, Read)
 
-answer :: Words -> Answer
-answer words = Answer words []
+answer :: Words -> Fan Answer Question
+answer words = A (Answer words)
 
-question :: Words -> Question
-question words = Question words []
+question :: Words -> Fan Answer Question
+question words = B (Question words)
 
-ask :: Answer -> Question -> Answer
-ask (Answer words questions) question = Answer words (question : questions)
+add :: Fan Answer Question -> Fan Answer Question -> Fan Answer Question
+add answer@(A _) question@(B _) = Fan [answer] [question]
+add (Fan [answer] questions) question@(Fan _ [one]) = Fan [answer] (question : questions)
+add (Fan answers [question]) answer@(Fan [one] _) = Fan (answer : answers) [question]
 
-reply :: Question -> Answer -> Question
-reply (Question words answers) answer = Question words (answer : answers)
 
-main = interact play
+main = do
+  createFile "db.txt"
+  contents <- readFile "db.txt"
+  let remembered = recall contents
+  putStrLn (show remembered)
+  input <- getLine
+  let heard = comprehend input
+  putStrLn (show heard)
+  writeFile "db.txt" . show $ add heard remembered
+  main
 
-play :: String -> String
-play = unlines . map (\xs -> "------------") . lines   
+comprehend :: Words -> Fan Answer Question
+comprehend ('?' : words) = question words
+comprehend words = answer words
+
+
+
+recall :: String -> Fan Answer Question
+recall contents
+  | contents == "" = ClosedFan
+  | otherwise = read contents
+
+createFile :: String -> IO ()
+createFile file = do
+  fileExist <- doesFileExist file
+  if not fileExist
+    then writeFile file ""
+    else return ()
+ 
