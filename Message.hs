b304c1d8d41626c454b79d4c777ba7982c44f0d9
Alexander Novikov <mem-memov@yandex.ru>
2019-09-04 09:17:14 +0300

    return not working

--- Message.hs
+++ Message.hs
@@ -33,41 +33,28 @@ findQuestion words questions =
     (Nothing, []) 
     questions
 
+makeInquiry :: Maybe Question -> Words -> [Question] -> Message
+makeInquiry answerQuestion answerWords answerQuestions = Inquiry (Answer answerQuestion answerWords answerQuestions)
+
+makeReference :: Maybe Answer -> Words -> [Answer] -> Message
+makeReference questionAnswer questionWords questionAnswers = Reference (Question questionAnswer questionWords questionAnswers)
+
 add :: Message -> Message -> Message
 
 add (Reference question@(Question questionAnswer questionWords questionAnswers)) (Inquiry answer@(Answer Nothing answerWords [])) = 
   case (findAnswer answerWords questionAnswers) of
     (Just (Answer foundAnswerQuestion foundAnswerWords foundAnswerQuestions), otherAnswers) -> 
-      Inquiry (
-        Answer 
-          (Just question)
-          foundAnswerWords 
-          foundAnswerQuestions
-      )
+      makeInquiry (Just question) foundAnswerWords foundAnswerQuestions
     (Nothing,  allAnswers) -> 
-      Reference (
-        Question 
-          questionAnswer
-          questionWords 
-          (answer : questionAnswers)
-      )
+      makeReference questionAnswer questionWords (answer : questionAnswers)
+
 
 add (Inquiry answer@(Answer answerQuestion answerWords answerQuestions)) (Reference question@(Question Nothing questionWords [])) =
   case (findQuestion questionWords answerQuestions) of
     (Just (Question foundQuestionAnswer foundQuestionWords foundQuestionAnswers), otherQuestions) -> 
-      Reference (
-        Question 
-          (Just answer)
-          foundQuestionWords 
-          foundQuestionAnswers
-      )
+      makeReference (Just answer) foundQuestionWords foundQuestionAnswers
     (Nothing, allQuestions) -> 
-      Inquiry (
-        Answer 
-          answerQuestion
-          answerWords 
-          (question : answerQuestions)
-      )
+      makeInquiry answerQuestion answerWords (question : answerQuestions)
 
 add message Silence = message 
 add Silence reference@(Reference _) = reference
@@ -83,29 +70,29 @@ showAnswer (Answer _ answerWords _) = (getWords answerWords)
 
 showQuestions :: [Question] -> String
 showQuestions [] = ""
-showQuestions (question : otherQuestions) = (showQuestion question) ++ "\n" ++ (showQuestions otherQuestions)
+showQuestions (question : otherQuestions) = "  " ++ (showQuestion question) ++ "\n" ++ (showQuestions otherQuestions)
 
 showAnswers :: [Answer] -> String
 showAnswers [] = ""
-showAnswers (answer : otherAnswers) = (showAnswer answer) ++ "\n" ++ (showAnswers otherAnswers)
+showAnswers (answer : otherAnswers) = "  " ++ (showAnswer answer) ++ "\n" ++ (showAnswers otherAnswers)
 
 showAnswerFocus :: Answer -> String
-showAnswerFocus answer@(Answer Nothing _ answerQuestions) = (showAnswer answer) ++ "\n" ++ (showQuestions answerQuestions)
-showAnswerFocus answer@(Answer (Just answerQuestion) _ answerQuestions) = (showQuestion answerQuestion) ++ "\n" ++ (showAnswer answer) ++ "\n" ++ (showQuestions answerQuestions)
+showAnswerFocus answer@(Answer Nothing _ answerQuestions) = " -> " ++ (showAnswer answer) ++ "\n" ++ (showQuestions answerQuestions)
+showAnswerFocus answer@(Answer (Just answerQuestion) _ answerQuestions) = (showQuestion answerQuestion) ++ "\n -> " ++ (showAnswer answer) ++ "\n" ++ (showQuestions answerQuestions)
 
 showQuestionFocus :: Question -> String
-showQuestionFocus question@(Question Nothing _ questionAnswers) = (showQuestion question) ++ "\n" ++ (showAnswers questionAnswers)
-showQuestionFocus question@(Question (Just questionAnswer) _ questionAnswers) = (showAnswer questionAnswer) ++ "\n" ++ (showQuestion question) ++ "\n" ++ (showAnswers questionAnswers)
+showQuestionFocus question@(Question Nothing _ questionAnswers) = " -> " ++ (showQuestion question) ++ "\n" ++ (showAnswers questionAnswers)
+showQuestionFocus question@(Question (Just questionAnswer) _ questionAnswers) = (showAnswer questionAnswer) ++ "\n -> " ++ (showQuestion question) ++ "\n" ++ (showAnswers questionAnswers)
 
 showFocus :: Message -> String
 showFocus Silence = "\n-"
-showFocus (Reference question) = "\n->" ++ showQuestionFocus question
-showFocus (Inquiry answer) = "\n->" ++ showAnswerFocus answer
+showFocus (Reference question) = "\n" ++ showQuestionFocus question
+showFocus (Inquiry answer) = "\n" ++ showAnswerFocus answer
 
 comprehend :: String -> Message
 comprehend "" = Silence
-comprehend ('?' : words) = Reference (Question Nothing (Words words) [])
-comprehend words = Inquiry (Answer Nothing (Words words) [])
+comprehend ('?' : words) = makeReference Nothing (Words words) []
+comprehend words = makeInquiry Nothing (Words words) []
 
 recall :: String -> Message
 recall contents
